1531be8e6e53cc91bde2e7808be450eff34a82ce
LuuMinhHanhNguyen <luuminhhanhnguyen@gmail.com>
2023-06-08 09:49:16 +0700

    hanh update lan 2

--- merch/src/main/java/com/example/merch/controller/team4/DAO/order_details/IOrderDetailsDAO.java
+++
@@ -1,7 +0,0 @@
-package com.example.team4.DAO.order_details;
-
-import com.example.team4.model.OrderDetails;
-
-public interface IOrderDetailsDAO {
-    void addNewOrderDetails(int orderId, OrderDetails orderDetails);
-}
